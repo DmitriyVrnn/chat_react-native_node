72f4e2069e57fc7c2b455dbb1d09599d2cb9af97
Dmitriy <kaishien777@gmail.com>
2020-04-02 19:22:13 +0300

    added bottom navigation

---
+++ chat/navigation/AppBottomNavigation.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { ChatScreen } from "../screens/ChatsScreen";
+import { Ionicons } from "@expo/vector-icons";
+import { SettingsScreen } from "../screens/SettingsScreen";
+
+
+const BottomNavigator = createBottomTabNavigator();
+
+export const AppBottomNavigation = () => {
+  return(
+    <BottomNavigator.Navigator
+      tabBarOptions={{
+        inactiveTintColor: 'gray',
+        shifting: true,
+      }}
+    >
+      <BottomNavigator.Screen
+        name="Post"
+        component={ChatScreen}
+        options={{
+          tabBarIcon: (info) => <Ionicons name='ios-chatboxes' size={25} color={info.color}/>,
+          tabBarLabel: 'Chat'
+        }}
+      />
+      <BottomNavigator.Screen
+        name="Booked"
+        component={SettingsScreen}
+        options={{
+          tabBarIcon: (info) => <Ionicons name='ios-settings' size={25} color={info.color}/>,
+          tabBarLabel: 'Settings'
+        }}
+      />
+    </BottomNavigator.Navigator>
+  )
+};
