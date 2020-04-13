25e65eaf84cf733661c73d67afba1748d703c25a
Dmitriy <kaishien777@gmail.com>
2020-04-13 03:00:39 +0300

    Refactoring: user -> users

--- backend/src/components/user/user.model.ts
+++ backend/src/components/user/user.model.ts
@@ -13,5 +13,5 @@ export const userSchema = new Schema({
   }
 });
 
-const userModel = mongoose.model<User & mongoose.Document>('user', userSchema);
+const userModel = mongoose.model<User & mongoose.Document>('users', userSchema);
 export default userModel;
